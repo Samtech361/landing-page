5eb39a577277261675fb5fec63b23c4a4826a0ab
Samtech361 <nsamuel361@gmail.com>
2023-05-12 12:00:24 +0300

    additional edits

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
-const navId = document.getElementById("nav-menu"),
-    ToggleBtnId = document.getElementById("toggle-btn"),
-    CloseBtnId = document.getElementById("close-btn")
-
+const navMenu = document.querySelector(".nav-menu");
+const toggleBtn = document.querySelector("#toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
 
 //show the menu
-ToggleBtnId.addEventListener("click", () =>{
-    navId.classList.add("show");
+ToggleBtnId.addEventListener("click", () => {
+  navId.classList.add("show");
+});
+
+//hide menu
+CloseBtnId.addEventListener("click", () => {
+  navId.classList.remove("show");
+});
+
+AOS.init();
+
+gsap.from(".logo", {
+  opacity: 0,
+  y: -10,
+  delay: 1,
+  duration: 0.5,
+});
+
+gsap.from(".nav-menu-list .nav-menu-item", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+  stagger: 0.3,
+});
+
+gsap.from(".toggle_btn", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+});
+
+gsap.from(".main-heading", {
+  opacity: 0,
+  y: 20,
+  delay: 2.4,
+  duration: 1,
+});
+
+gsap.from(".info-text", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+
+gsap.from(".btn_wrapper", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+
+gsap.from(".team_img_wrapper img", {
+  opacity: 0,
+  y: 20,
+  delay: 3,
+  duration: 1,
 });
